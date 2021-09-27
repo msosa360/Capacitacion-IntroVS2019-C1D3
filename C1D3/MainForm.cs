e20ddbec05e399115b2bfb15703252cac8cab801
Maximiliano Sosa <msosa_@hotmail.com>
2021-09-27 17:52:18 -0300

    end

---
+++ C1D3/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Admin;
+
+namespace C1D3
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            Persistence.UsuariosRepository.LoadTestData();
+        }
+
+        private void btnAltaUsuario_Click(object sender, EventArgs e)
+        {
+            ManejoUsuariosForm form = new ManejoUsuariosForm();
+            form.ShowDialog();
+        }
+    }
+}
---
+++ Persistence/Usuarios.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain;
+
+namespace Persistence
+{
+    public static class UsuariosRepository
+    {
+        private static List<Usuario> Usuarios { get; set; } = new List<Usuario>();
+
+        public static void Add(Usuario usuario)
+        {
+            if (Usuarios.Contains(usuario))
+                throw new Exception("El usuario ya existe");
+
+            Usuarios.Add(usuario);
+        }
+
+        public static void Update(Usuario usuario)
+        {
+            if (!Usuarios.Contains(usuario))
+                throw new Exception("El usuario no existe");
+
+            Usuario usuarioExistente = Usuarios.First(x => x.Login == usuario.Login);
+
+            usuarioExistente.Clave = usuario.Clave;
+            usuarioExistente.Nombre = usuario.Nombre;
+        }
+
+
+        public static void Delete(Usuario usuario)
+        {
+            if (!Usuarios.Contains(usuario))
+                throw new Exception("El usuario no existe");
+
+            Usuarios.Remove(usuario);
+        }
+
+        public static List<Usuario> List()
+        {
+            return Usuarios;
+        }
+
+        public static void LoadTestData()
+        {
+            Usuarios.Add(new Usuario { Login = "admin", Nombre = "Administrador", Clave = "1234" });
+            Usuarios.Add(new Usuario { Login = "usr1", Nombre = "Usuario 1", Clave = "1234" });
+            Usuarios.Add(new Usuario { Login = "usr2", Nombre = "Usuario 2", Clave = "1234" });
+        }
+    }
+}
